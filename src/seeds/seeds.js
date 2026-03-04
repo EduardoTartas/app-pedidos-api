4e0ab6811b439ac2a3542a452bc6a5d1db4f4bfe
eduardotartas <eduardos.tartas@gmail.com>
2026-03-03 21:53:22 -0400

    feat: adicionar scripts de seed para usuários com dados iniciais

---
+++ src/seeds/seeds.js
@@ -0,0 +1,28 @@
+// src/seeds/seeds.js
+
+import 'dotenv/config';
+import mongoose from 'mongoose';
+import seedUsuarios from './seedsUsuario.js';
+//import seedCategorias from './seedsCategoria.js';
+//import seedRestaurantes from './seedsRestaurante.js';
+//import seedPratos from './seedsPrato.js';
+//import seedAdicionais from './seedsAdicional.js';
+
+async function main() {
+    try {
+        //await seedCategorias();
+        await seedUsuarios();
+        //await seedRestaurantes();
+        //await seedPratos();
+        //await seedAdicionais();
+
+        console.log('>>> SEED FINALIZADO COM SUCESSO! <<<');
+    } catch (err) {
+        console.error('Erro ao executar SEED:', err);
+    } finally {
+        mongoose.connection.close();
+        process.exit(0);
+    }
+}
+
+main();
