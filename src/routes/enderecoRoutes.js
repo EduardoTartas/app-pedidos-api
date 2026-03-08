be7ed91d68b5f49c53d299d3da86804bdb760d4d
EduardoTartas <eduardos.tartas@gmail.com>
2026-03-08 18:37:05 -0400

    feat(auth): adicionando  AuthMiddleware nas rotas endereco, restaurante e usuario

--- src/routes/enderecoRoutes.js
+++ src/routes/enderecoRoutes.js
@@ -3,7 +3,7 @@
 import express from 'express';
 import EnderecoController from '../controllers/EnderecoController.js';
 import { asyncWrapper } from '../utils/helpers/index.js';
-// import AuthMiddleware from '../middlewares/AuthMiddleware.js';
+import AuthMiddleware from '../middlewares/AuthMiddleware.js';
 
 const router = express.Router();
 
@@ -11,16 +11,17 @@ const enderecoController = new EnderecoController();
 
 // Rotas de endereço de usuário (um usuário pode ter múltiplos endereços)
 router
-    .get('/usuarios/:usuarioId/enderecos', /*AuthMiddleware,*/ asyncWrapper(enderecoController.listarPorUsuario.bind(enderecoController)))
-    .post('/usuarios/:usuarioId/enderecos', /*AuthMiddleware,*/ asyncWrapper(enderecoController.criarParaUsuario.bind(enderecoController)))
-    .patch('/usuarios/:usuarioId/enderecos/:enderecoId', /*AuthMiddleware,*/ asyncWrapper(enderecoController.atualizarDeUsuario.bind(enderecoController)))
-    .delete('/usuarios/:usuarioId/enderecos/:enderecoId', /*AuthMiddleware,*/ asyncWrapper(enderecoController.deletarDeUsuario.bind(enderecoController)));
+    .get('/usuarios/:usuarioId/enderecos', AuthMiddleware, asyncWrapper(enderecoController.listarPorUsuario.bind(enderecoController)))
+    .post('/usuarios/:usuarioId/enderecos', AuthMiddleware, asyncWrapper(enderecoController.criarParaUsuario.bind(enderecoController)))
+    .patch('/usuarios/:usuarioId/enderecos/:enderecoId', AuthMiddleware, asyncWrapper(enderecoController.atualizarDeUsuario.bind(enderecoController)))
+    .delete('/usuarios/:usuarioId/enderecos/:enderecoId', AuthMiddleware, asyncWrapper(enderecoController.deletarDeUsuario.bind(enderecoController)));
 
 // Rotas de endereço de restaurante (um restaurante pode ter somente um endereço)
 router
     .get('/restaurantes/:restauranteId/enderecos', asyncWrapper(enderecoController.buscarPorRestaurante.bind(enderecoController)))
-    .post('/restaurantes/:restauranteId/enderecos', /*AuthMiddleware,*/ asyncWrapper(enderecoController.criarParaRestaurante.bind(enderecoController)))
-    .patch('/restaurantes/:restauranteId/enderecos/:enderecoId', /*AuthMiddleware,*/ asyncWrapper(enderecoController.atualizarDeRestaurante.bind(enderecoController)))
-    .delete('/restaurantes/:restauranteId/enderecos/:enderecoId', /*AuthMiddleware,*/ asyncWrapper(enderecoController.deletarDeRestaurante.bind(enderecoController)));
+    .post('/restaurantes/:restauranteId/enderecos', AuthMiddleware, asyncWrapper(enderecoController.criarParaRestaurante.bind(enderecoController)))
+    .patch('/restaurantes/:restauranteId/enderecos/:enderecoId', AuthMiddleware, asyncWrapper(enderecoController.atualizarDeRestaurante.bind(enderecoController)))
+    .delete('/restaurantes/:restauranteId/enderecos/:enderecoId', AuthMiddleware, asyncWrapper(enderecoController.deletarDeRestaurante.bind(enderecoController)));
 
 export default router;
+ 
--- src/routes/restauranteRoutes.js
+++ src/routes/restauranteRoutes.js
@@ -3,7 +3,7 @@
 import express from 'express';
 import RestauranteController from '../controllers/RestauranteController.js';
 import { asyncWrapper } from '../utils/helpers/index.js';
-// import AuthMiddleware from '../middlewares/AuthMiddleware.js';
+import AuthMiddleware from '../middlewares/AuthMiddleware.js';
 
 const router = express.Router();
 
@@ -12,8 +12,8 @@ const restauranteController = new RestauranteController();
 router
     .get('/restaurantes', asyncWrapper(restauranteController.listar.bind(restauranteController)))
     .get('/restaurantes/:id', asyncWrapper(restauranteController.listar.bind(restauranteController)))
-    .post('/restaurantes', /*AuthMiddleware,*/ asyncWrapper(restauranteController.criar.bind(restauranteController)))
-    .patch('/restaurantes/:id', /*AuthMiddleware,*/ asyncWrapper(restauranteController.atualizar.bind(restauranteController)))
-    .delete('/restaurantes/:id', /*AuthMiddleware,*/ asyncWrapper(restauranteController.deletar.bind(restauranteController)));
+    .post('/restaurantes', AuthMiddleware, asyncWrapper(restauranteController.criar.bind(restauranteController)))
+    .patch('/restaurantes/:id', AuthMiddleware, asyncWrapper(restauranteController.atualizar.bind(restauranteController)))
+    .delete('/restaurantes/:id', AuthMiddleware, asyncWrapper(restauranteController.deletar.bind(restauranteController)));
 
 export default router;
